c2401b131ac53a68f143c0d6c70caefbfb3829ff
animeslayer8 <126319219+animeslayer8@users.noreply.github.com>
2023-03-24 07:08:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
 
-# for thumbnail ( back end is MrMKN brain 😉)
-DOWNLOAD_LOCATION = "./DOWNLOADS"
+# for thumbnail ( back end is warlords )
+      DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+
 
